8c6bcd53320e96d8571793296181ab5ac5d74897
tokhi <shafi.tokhi@gmail.com>
2013-03-25 12:45:13 +0430

    adding input

--- Identity.java
+++ Identity.java
@@ -1,6 +1,11 @@
+import java.util.*;
 public class Identity{
 
 	public static void main(String[] args){
-		System.out.println("Hello to Github")
+		System.out.println("Hello to Github");
+		System.out.println("Enter your Name:");
+		Scanner input = new Scanner(System.in);
+		name = input.next();
+		System.out.println(name);
 	}
 }
